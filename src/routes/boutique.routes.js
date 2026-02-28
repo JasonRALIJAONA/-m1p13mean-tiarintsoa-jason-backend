97abfc41172ecb417363b23427dbeba5983c0f85
Tiarintsoa <rihantiana000@gmail.com>
2026-02-28 11:07:53 +0300

    [feature] Shop CRUD auth protection

--- src/routes/boutique.routes.js
+++ src/routes/boutique.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const boutiqueController = require('../controllers/boutique.controller');
+const { auth, authorize } = require('../middlewares/auth.middleware');
 
 // GET all shops
 router.get('/', boutiqueController.getAllBoutiques);
@@ -15,12 +16,12 @@ router.get('/statut/:statut', boutiqueController.getBoutiquesByStatut);
 router.get('/:id', boutiqueController.getBoutiqueById);
 
 // POST create a new shop
-router.post('/', boutiqueController.createBoutique);
+router.post('/', auth, authorize(['admin']), boutiqueController.createBoutique);
 
 // PUT update a shop
-router.put('/:id', boutiqueController.updateBoutique);
+router.put('/:id', auth, authorize(['admin']), boutiqueController.updateBoutique);
 
 // DELETE a shop
-router.delete('/:id', boutiqueController.deleteBoutique);
+router.delete('/:id', auth, authorize(['admin']), boutiqueController.deleteBoutique);
 
 module.exports = router;
